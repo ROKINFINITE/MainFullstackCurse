822ac0fe5f96137fa7b28a6e57809e8736d5a434
ROKINFINITE <manriquegaxiola1234@gmail.com>
2019-03-09 10:48:11 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Main fullstack curse 
---
+++ script.js
@@ -0,0 +1,6 @@
+function changetitle() {
+    var title = document.getElementById('title');
+    var firstname = document.getElementById('firstname');
+    
+    title.innerHTML = 'HOLA ' + firstname.value;
+} 
